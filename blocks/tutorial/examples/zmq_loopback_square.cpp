280fe62792f90dd9f3163ee055b82c88300057cf
Josh Morman <jmorman@gnuradio.org>
2025-08-23 19:44:46 +0000

    Lesson 1 updates
    Signed-off-by: Josh Morman <jmorman@gnuradio.org>

---
+++ blocks/tutorial/examples/zmq_loopback_square.cpp
@@ -0,0 +1,62 @@
+#include <complex>
+#include <cstdint>
+#include <cstdlib>
+
+#include <gnuradio-4.0/Graph.hpp>
+#include <gnuradio-4.0/Scheduler.hpp>
+#include <gnuradio-4.0/PluginLoader.hpp>
+#include <gnuradio-4.0/tutorial/Square.hpp>
+#include <gnuradio-4.0/zeromq/ZmqPushSink.hpp>
+#include <gnuradio-4.0/zeromq/ZmqPullSource.hpp>
+
+#include <gnuradio-4.0/BlockRegistry.hpp>
+
+#include <print>
+
+using namespace gr;
+using namespace gr::tutorial;
+
+int main() {
+
+ 
+    using T = float;
+
+    gr::Graph fg;
+    auto&     source = fg.emplaceBlock<gr::zeromq::ZmqPullSource<T>>({
+        {"endpoint", "tcp://localhost:5555"},
+        {"timeout", 10},
+        {"bind", false},
+    });
+
+    auto& sink = fg.emplaceBlock<gr::zeromq::ZmqPushSink<T>>({
+        {"endpoint", "tcp://localhost:5556"},
+        {"timeout", 100},
+        {"bind", true},
+    });
+
+    auto& squareBlock = fg.emplaceBlock<Square>({
+    });
+
+
+    const char* connection_error = "connection_error";
+
+    // if (fg.connect<"out">(source).to<"in">(sink) != gr::ConnectionResult::SUCCESS) {
+    //     throw gr::exception(connection_error);
+    // }
+    if (fg.connect<"out">(source).to<"in">(squareBlock) != gr::ConnectionResult::SUCCESS) {
+        throw gr::exception(connection_error);
+    }
+    if (fg.connect<"out">(squareBlock).to<"in">(sink) != gr::ConnectionResult::SUCCESS) {
+        throw gr::exception(connection_error);
+    }
+
+
+    gr::scheduler::Simple<gr::scheduler::ExecutionPolicy::singleThreaded> sched{std::move(fg)};
+    const auto                                                            ret = sched.runAndWait();
+    if (!ret.has_value()) {
+        std::print("scheduler error: {}", ret.error());
+        std::exit(1);
+    }
+
+    return 0;
+}
